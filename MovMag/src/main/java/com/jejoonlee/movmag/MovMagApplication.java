a112066787f33954a755c8fe534d210fcdf6dca2
jejoonlee <jejoonlee1996@gmail.com>
2023-10-20 11:31:06 +0900

    README 업데이트

--- MovMag/src/main/java/com/jejoonlee/movmag/MovMagApplication.java
+++ MovMag/src/main/java/com/jejoonlee/movmag/MovMagApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MovMagApplication {
 
 	public static void main(String[] args) {
-		System.out.println("test");
 		SpringApplication.run(MovMagApplication.class, args);
 	}
 
